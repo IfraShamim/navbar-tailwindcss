b0fc6f109b7c6c87f7d29ef66e2739ae492cce9f
Ifra Shamim <ifrashamim29@gmail.com>
2024-12-14 16:52:16 +0500

    Done!

--- sw.js
+++ sw.js
@@ -1,21 +1,42 @@
-const CACHE_NAME = "my-pwa-cache-v1";
-const urlsToCache = ["/", "/index.html", "/style.css", "/script.js"];
+const CACHE_NAME = "navbar-tailwindcss-cache-v1";
+const urlsToCache = [
+  "/navbar-tailwindcss/",
+  "/navbar-tailwindcss/index.html",
+  "/navbar-tailwindcss/style.css",
+  "/navbar-tailwindcss/script.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+];
 
-// Install event
+// Install Service Worker
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log("Opened cache");
       return cache.addAll(urlsToCache);
     })
   );
 });
 
-// Fetch event
+// Fetch Requests
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate Service Worker
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
